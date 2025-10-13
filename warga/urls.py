e440ba6e8dff70a4f488aa233846c478b5ba9278
galangalp <galangb03@gmail.com>
2025-10-13 22:37:15 +0700

    Tugas Framewrok Pertemuan 2

--- warga/urls.py
+++ warga/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import WargaListView, WargaDetailListView
+from .views import WargaListView, WargaDetailListView, PengaduanListView
 
 urlpatterns = [
     path('', WargaListView.as_view(), name='warga-list'),
     path('<int:pk>/', WargaDetailListView.as_view(), name='warga-detail'),
+    path('pengaduan/', PengaduanListView.as_view(), name='pengaduan-list'),
 ]
--- warga/views.py
+++ warga/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import ListView,DetailView
-from .models import Warga
+from .models import Warga, Pengaduan
 
 class WargaListView(ListView):
     model = Warga
 class WargaDetailListView(DetailView):
     model = Warga
+class PengaduanListView(ListView):
+    model = Pengaduan
