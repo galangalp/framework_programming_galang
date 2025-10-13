32aa74b0840b0f2fac0e6ed30c7c1b741887915d
galangalp <galangb03@gmail.com>
2025-10-13 23:14:32 +0700

    Tugas Framework Pertemuan 3

--- warga/urls.py
+++ warga/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import WargaListView, WargaDetailListView, PengaduanListView
-
+from .views import WargaListView, WargaDetailListView, PengaduanListView, WargaCreateView, PengaduanCreateView
 urlpatterns = [
     path('', WargaListView.as_view(), name='warga-list'),
     path('<int:pk>/', WargaDetailListView.as_view(), name='warga-detail'),
     path('pengaduan/', PengaduanListView.as_view(), name='pengaduan-list'),
+    path('tambah/', WargaCreateView.as_view(), name='warga-tambah'),
+    path('pengaduan/tambah/', PengaduanCreateView.as_view(), name='pengaduan-tambah'),
 ]
